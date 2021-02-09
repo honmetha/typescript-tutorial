47b7a9cbb4233cc687b6c95b7d61ce79513c3002
Hon <dennis.mejen@gmail.com>
2021-02-09 23:28:58 +0700

    [Added] TS Tutorial #8 - Function Basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log('test');
+var greet;
+// greet = 'hello';
+greet = function () {
+    console.log('hello');
+};
+var add = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, '20');
+var minus = function (a, b) {
+    return a - b;
+};
+var result = minus(10, 7);
+// result = 'something else';
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,21 @@
-console.log('test');
+let greet: Function;
+
+// greet = 'hello';
+
+greet = () => {
+  console.log('hello');
+};
+
+const add = (a: number, b: number, c: number | string = 10): void => {
+  console.log(a + b);
+  console.log(c);
+};
+
+add(5, 10, '20');
+
+const minus = (a: number, b: number): number => {
+  return a - b;
+};
+
+let result = minus(10, 7);
+// result = 'something else';
