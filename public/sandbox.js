86ada8d2deb21862adbe5f4c9d3ac0a693677370
Hon <dennis.mejen@gmail.com>
2021-02-10 23:40:18 +0700

    [Added] TS Tutorial #10 - Function Signatures

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,10 +1,22 @@
 "use strict";
-var logDetails = function (uid, item) {
-    console.log(item + " has a uid of " + uid);
+// let greet: Function;
+// example 1
+var greet;
+greet = function (name, greeting) {
+    console.log(name + " says " + greeting);
 };
-var greet = function (user) {
-    console.log(user.name + " says hello");
+// example 2
+var calc;
+calc = function (numOne, numTwo, action) {
+    if (action === 'add') {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo;
+    }
 };
-var greetAgain = function (user) {
-    console.log(user.name + " says hello");
+// example 3
+var logDetails;
+logDetails = function (ninja) {
+    console.log(ninja.name + " is " + ninja.age + " years old");
 };
