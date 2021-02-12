5abc83c38732eac1028956052c78d88176c94f20
Hon <dennis.mejen@gmail.com>
2021-02-12 21:55:40 +0700

    [Added] TS Tutorial #12 - Classes

--- public/app.js
+++ public/app.js
@@ -1,10 +1,29 @@
 "use strict";
-// const anchor = document.querySelector('a')!;
-// if (anchor) console.log(anchor.href);
-// console.log(anchor.href);
-// const form = document.querySelector('form')!;
+// classes
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    ;
+    Invoice.prototype.format = function () {
+        return this.client + " owes " + this.amount + " for " + this.details;
+    };
+    ;
+    return Invoice;
+}());
+;
+var invOne = new Invoice('mario', 'work on the mario website', 250);
+var invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+var invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invOne.client = 'yoshi';
+invTwo.amount = 400;
+console.log(invOne, invTwo);
+console.log(invoices);
 var form = document.querySelector('.new-item-form');
-// console.log(form.children);
 // inputs
 var type = document.querySelector('#type');
 var tofrom = document.querySelector('#tofrom');
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,36 @@
-// const anchor = document.querySelector('a')!;
+// classes
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
 
-// if (anchor) console.log(anchor.href);
+  constructor(c: string, d: string, a: number) {
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  };
 
-// console.log(anchor.href);
+  format() {
+    return `${this.client} owes ${this.amount} for ${this.details}`;
+  };
+};
 
-// const form = document.querySelector('form')!;
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+
+let invoices: Invoice[] = [];
+
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invOne.client = 'yoshi';
+invTwo.amount = 400;
+
+console.log(invOne, invTwo);
+
+console.log(invoices);
+
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-// console.log(form.children);
 
 // inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
