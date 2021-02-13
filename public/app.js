fedb6f891851705006e37c3fcf9229e5051f434c
Hon <dennis.mejen@gmail.com>
2021-02-13 23:29:43 +0700

    [Added] TS Tutorial #13 - Public, Private & Readonly

--- public/app.js
+++ public/app.js
@@ -1,10 +1,13 @@
 "use strict";
 // classes
 var Invoice = /** @class */ (function () {
-    function Invoice(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
+    // readonly client: string;
+    // private details: string;
+    // public amount: number;
+    function Invoice(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
     }
     ;
     Invoice.prototype.format = function () {
@@ -19,10 +22,9 @@ var invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 var invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
-invOne.client = 'yoshi';
-invTwo.amount = 400;
-console.log(invOne, invTwo);
-console.log(invoices);
+invoices.forEach(function (inv) {
+    console.log(inv.client, inv.amount, inv.format());
+});
 var form = document.querySelector('.new-item-form');
 // inputs
 var type = document.querySelector('#type');
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 // classes
 class Invoice {
-  client: string;
-  details: string;
-  amount: number;
+  // readonly client: string;
+  // private details: string;
+  // public amount: number;
 
-  constructor(c: string, d: string, a: number) {
-    this.client = c;
-    this.details = d;
-    this.amount = a;
-  };
+  constructor(
+    readonly client: string,
+    private details: string,
+    public amount: number,
+  ) {};
 
   format() {
     return `${this.client} owes ${this.amount} for ${this.details}`;
@@ -19,16 +19,12 @@ const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 
 let invoices: Invoice[] = [];
-
 invoices.push(invOne);
 invoices.push(invTwo);
 
-invOne.client = 'yoshi';
-invTwo.amount = 400;
-
-console.log(invOne, invTwo);
-
-console.log(invoices);
+invoices.forEach(inv => {
+  console.log(inv.client, inv.amount, inv.format());
+});
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
