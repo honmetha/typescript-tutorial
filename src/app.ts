2cd1cff1e9fd256ffe649b83fee7aa620a7cfea8
Hon <dennis.mejen@gmail.com>
2021-02-14 23:26:18 +0700

    [Added] TS Tutorial #14 - Modules

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,4 @@
-// classes
-class Invoice {
-  // readonly client: string;
-  // private details: string;
-  // public amount: number;
-
-  constructor(
-    readonly client: string,
-    private details: string,
-    public amount: number,
-  ) {};
-
-  format() {
-    return `${this.client} owes ${this.amount} for ${this.details}`;
-  };
-};
+import { Invoice } from './classes/Invoice.js';
 
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
